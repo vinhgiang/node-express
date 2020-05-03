2a7a59482947a8a5c4e8562ee5645e3177fdd93b
Kevin <giangcamvinh@gmail.com>
2020-05-03 16:05:19 -0700

    apply unit test

--- meadowlark.js
+++ meadowlark.js
@@ -1,35 +1,35 @@
-const express = require('express')
-const expressHandlebars = require('express-handlebars')
+const express = require('express');
+const expressHandlebars = require('express-handlebars');
 
-const handlers = require('./lib/handlers')
+const handlers = require('./lib/handlers');
 
-const app = express()
+const app = express();
 
 // configure Handlebars view engine
 app.engine('handlebars', expressHandlebars({
   defaultLayout: 'main',
-}))
-app.set('view engine', 'handlebars')
+}));
+app.set('view engine', 'handlebars');
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', handlers.home)
+app.get('/', handlers.home);
 
-app.get('/about', handlers.about)
+app.get('/about', handlers.about);
 
 // custom 404 page
-app.use(handlers.notFound)
+app.use(handlers.notFound);
 
 // custom 500 page
-app.use(handlers.serverError)
+app.use(handlers.serverError);
 
 if(require.main === module) {
   app.listen(port, () => {
     console.log( `Express started on http://localhost:${port}` +
-      '; press Ctrl-C to terminate.' )
-  })
+      '; press Ctrl-C to terminate.' );
+  });
 } else {
-  module.exports = app
+  module.exports = app;
 }
